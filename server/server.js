7ee4cda82d2b7d8f08879544bdd88934f912735f
Dimitri Wahyudiputra <deathmitri@gmail.com>
2017-11-02 16:38:23 +0700

    Added server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+const port = process.env.PORT || 3000;
+
+server.use(cors());
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.use((req, res) => {
+  res.status(404).json({
+    error: 'Page not found'
+  });
+});
+
+server.listen(port, () => {
+  console.log('Click War Server running on port', port);
+});
