fb3316309804b1bffd6754e6650b96e4e186dd50
lokiloko <ianw.tkj2@gmail.com>
2017-11-03 11:11:10 +0700

    setup initialization for client

--- firebase-client/src/main.js
+++ firebase-client/src/main.js
@@ -2,7 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+// import Firebase from 'firebase'
+import VueFire from 'vuefire'
 import router from './router'
+import store from './vuex/store'
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+Vue.use(VueFire)
 
 Vue.config.productionTip = false
 
@@ -10,6 +20,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
